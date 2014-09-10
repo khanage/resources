fb93f74e56bccab8ce264fbde645cce074099576
Khan Thompson <khanage@gmail.com>
2014-09-10 14:15:55 +1000

    Store deb in reasonable location

--- install-sbt.sh
+++ install-sbt.sh
@@ -1,8 +1,8 @@
 #scala installation
-wget www.scala-lang.org/files/archive/scala-2.11.1.deb
-sudo dpkg -i scala-2.11.1.deb
+sudo wget www.scala-lang.org/files/archive/scala-2.11.1.deb -O /tmp/scala-2.11.1.deb
+sudo dpkg -i /tmp/scala-2.11.1.deb
 
 
 # sbt installation
-wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.1/sbt.deb
-sudo dpkg -i sbt.deb
+sudo wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.1/sbt.deb -O /tmp/sbt.deb
+sudo dpkg -i /tmp/sbt.deb
